0f6fa2418e85358af1c948ab8c5034a0c0b4b603
ebert.l <info@leon-ebert.de>
2025-03-22 15:54:31 +0100

    Added the Test Structure

---
+++ src/test/java/de/vfh/unifile/uf_conflict/UF_ConflictServiceTest.java
@@ -0,0 +1,15 @@
+package de.vfh.unifile.uf_conflict;
+
+import org.junit.jupiter.api.Test;
+
+public class UF_ConflictServiceTest {
+    @Test
+    void testAnalyzeConflicts() {
+
+    }
+
+    @Test
+    void testSetMerge() {
+
+    }
+}
